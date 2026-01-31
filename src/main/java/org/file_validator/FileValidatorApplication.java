0257f5fe107772db246fb313ed56087bfe967016
kedar000 <140879080+kedar000@users.noreply.github.com>
2026-01-31 16:21:23 +0530

    first commit

---
+++ src/main/java/org/file_validator/FileValidatorApplication.java
@@ -0,0 +1,13 @@
+package org.file_validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileValidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileValidatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/file_validator/FileValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.file_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
